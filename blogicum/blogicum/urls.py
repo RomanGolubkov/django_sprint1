fc4594d97c24f2cb3670617b0c5c0d5fa65be7e9
Роман Голубков <Golubkov3112@yandex.ru>
2024-04-05 11:54:54 +0300

    Review changes

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
-from blog.views import handler404, handler500
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('pages.urls')),
     path('', include('blog.urls')),
 ]
-
-handler404 = handler404
-handler500 = handler500
